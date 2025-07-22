b37c84577370635280c0c4190bd9b67bf6d86f7c
WilliamMatiasDeOLiveira <williammatiasjau@hotmail.com>
2025-07-22 16:59:50 -0300

    ajuste de responsivfidade da pagina sobre

--- resources/views/Pages/sobre.blade.php
+++ resources/views/Pages/sobre.blade.php
@@ -3,100 +3,69 @@
 
 @section('content')
 
-    <style>
-        .sobre {
-            margin-top: 3rem;
-            padding: 0 5%;
-            /* background-color: red; */
-        }
+    <div class="sobre-container">
 
-        .cultura{
-            display: flex;
-            justify-content: space-around;
-            padding: 0 5%;
-            /* background-color: yellow; */
-        }
+        <div class="cards">
+            <div>
+                <h2>Missão</h2>
+                <p>
+                    Facilitar a conexão entre empresas que buscam mão de obra qualificada e freelancers talentosos,
+                    oferecendo
+                    uma plataforma que valoriza a flexibilidade, a diversidade e a inovação, com foco na excelência,
+                    comprometimento e satisfação dos clientes
+                </p>
+            </div>
+            <div>
+                <h2>Visão</h2>
+                <p>
+                    Ser uma referência em soluções de serviços flexíveis e de qualidade, promovendo a colaboração e o
+                    desenvolvimento profissional de freelancers, gerando agilidade e excelência nos serviços prestados,
+                    conforme
+                    as necessidades dos clientes.
+                </p>
 
-        .cultura div{
-            width: 30%;
-            border: 1px solid silver;
-            padding: 2rem;
-            margin-top: 2rem;
-            border-radius: 1rem;
-
-        }
-
-        .cultura h2{
-            color: #ff7300;
-            font-size: 3rem;
-            font-weight: bold;
-            text-align: center;
-        }
-
-        .cultura p{
-            color: white;
-            font-size: 1.5rem;
-            text-align: justify;
-        }
-    </style>
-
-    <div class="cultura">
-        <div>
-            <h2>Missão</h2>
-            <p>
-                Facilitar a conexão entre empresas que buscam mão de obra qualificada e freelancers talentosos, oferecendo
-                uma plataforma que valoriza a flexibilidade, a diversidade e a inovação, com foco na excelência,
-                comprometimento e satisfação dos clientes
-            </p>
+            </div>
+            <div>
+                <h2>Valores</h2>
+                <p>
+                    Executar e satisfazer os clientes com excelência, dedicação com segurança e comprometimento.
+                </p>
+            </div>
         </div>
-        <div>
-            <h2>Visão</h2>
+
+        <div class="sobre">
+
             <p>
-                Ser uma referência em soluções de serviços flexíveis e de qualidade, promovendo a colaboração e o
-                desenvolvimento profissional de freelancers, gerando agilidade e excelência nos serviços prestados, conforme
-                as necessidades dos clientes.
+                Somos uma empresa no mercado de mão de obra, <span>focada em atender as demandas de pequenas escalas ou ate
+                    mesmo em
+                    grandes períodos</span>
+                Com foco no comprometimento, <span>agilidade e flexibilidade.</span> A cooperativa operará como uma
+                plataforma que conecta
+                empresas a mão de obra temporárias e freelancers em diversas áreas.
+            </p>
+            <br>
+            <p>
+                Fazendo que tenha acessos a grandes talentos em conexão direta, em pool de diversificadas atividades e
+                especializações. <span>Tendo contratações sob demanda.</span>
+            </p>
+            <br>
+            <p>
+                Permitindo os ajustes conforme necessidades, em foco de <span>economia em tempo e recursos.</span> Uma
+                empresa com capacidade
+                de atender projetos de diferentes escalas e complexidades, desde mão de obras diárias como serviços a longos
+                prazos.
+            </p>
+            <br>
+            <p>
+                A <span>Coop. Service</span>, se propõe a transformar a forma de prestação de serviço, <span>diminuindo os
+                    vínculos empregatícios e
+                    problemas administrativos,</span> criando um ecossistema que promove o desenvolvimento sustentável a uma
+                colaboração de
+                sucesso mútuo. Estamos comprometidos a redefinir o mercado de prestações de serviços, entregando valor a
+                freelancers e temporários quanto as empresas que contratam.
             </p>
 
         </div>
-        <div>
-            <h2>Valores</h2>
-            <p>
-                Executar e satisfazer os clientes com excelência, dedicação com segurança e comprometimento.
-            </p>
-        </div>
-    </div>
-
-    <div class="sobre">
-
-        <p>
-            Somos uma empresa no mercado de mão de obra, <span>focada em atender as demandas de pequenas escalas ou ate
-                mesmo em
-                grandes períodos</span>
-            Com foco no comprometimento, <span>agilidade e flexibilidade.</span> A cooperativa operará como uma
-            plataforma que conecta
-            empresas a mão de obra temporárias e freelancers em diversas áreas.
-        </p>
-        <br>
-        <p>
-            Fazendo que tenha acessos a grandes talentos em conexão direta, em pool de diversificadas atividades e
-            especializações. <span>Tendo contratações sob demanda.</span>
-        </p>
-        <br>
-        <p>
-            Permitindo os ajustes conforme necessidades, em foco de <span>economia em tempo e recursos.</span> Uma
-            empresa com capacidade
-            de atender projetos de diferentes escalas e complexidades, desde mão de obras diárias como serviços a longos
-            prazos.
-        </p>
-        <br>
-        <p>
-            A <span>Coop. Service</span>, se propõe a transformar a forma de prestação de serviço, <span>diminuindo os
-                vínculos empregatícios e
-                problemas administrativos,</span> criando um ecossistema que promove o desenvolvimento sustentável a uma
-            colaboração de
-            sucesso mútuo. Estamos comprometidos a redefinir o mercado de prestações de serviços, entregando valor a
-            freelancers e temporários quanto as empresas que contratam.
-        </p>
 
     </div>
 
